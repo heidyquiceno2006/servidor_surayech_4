b6678780ead32aff3fcda83fb0acb885da91c05b
heidy <heidyquiceno2@gmail.com>
2024-09-17 14:26:38 -0500

    se crea cambios

--- src/main/java/com/example/servidor_suratech_4/MODELOS/Paciente.java
+++ src/main/java/com/example/servidor_suratech_4/MODELOS/Paciente.java
@@ -1,2 +1,20 @@
-package com.example.servidor_suratech_4.MODELOS;public class Paciente {
+package com.example.servidor_suratech_4.MODELOS;
+
+import org.hibernate.loader.ast.spi.Loadable;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+    Integer id;
+    String nombre;
+    Integer edad;
+    LocalDate fechaNacimiento
+    String documento;
+    String telefono;
+    Integer copago;
+    String email;
+    String direccion;
+    String ips;
+    String medicoFamilia;
 }
